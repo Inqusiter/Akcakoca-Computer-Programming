0116c4608d18e104df7aaaed1016dcb0feb88ff7
Berat Karabulut <53434685+Inqusiter@users.noreply.github.com>
2021-12-15 18:39:55 +0300

    SimpleLoopExample1
    I messed up the order of the examples

--- SimpleLoopExamples/SimpleLoopExample1/Program.cs
+++ SimpleLoopExamples/SimpleLoopExample1/Program.cs
@@ -1,54 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleLoopExample1
+namespace SimpleLoopExample2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* Hello, we are making simple examples with using for.
-             * But here we will use something  we didn't learn at university.
-             * That method is
-             * 
-             * anythingtype: 
-             * 
-             * goto anythingtype:
-             * 
-             * We are using that function to direct to step.
-             * That function mission is starting our software again.
-             * We don't want to open this again.
-             * We have a chance to write Console.ReadKey(); but after the software will close suddenly. We need to open it again.
-             */
+            int Num1, Num2, Result = 0;
+            Console.WriteLine("Hello, I am Addition bot. I am programmed  to collect 10 values entered externally.");
 
-
-
-
-            int Value1 = 0, Value2 = 0, Result, Loop, Avarage ;
-
-            start:
-            Console.Write("Hello, I am avarage calculator bot. How many times should ı work ? :");
-            Loop = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < Loop; i++)
+            for (int i = 0; i < 10; i++)
             {
-                Console.Write("Please enter first value : ");
-                Value1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("First number:");
+                Num1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Please enter second value : ");
-                Value2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Second number:");
+                Num2 = Convert.ToInt32(Console.ReadLine());
 
-                Result = (Value1 + Value2) / 2 ;
-                Console.WriteLine("Avarege is :" + Result);
-                Console.WriteLine("I am working again...");
-
+                Result = Num1 + Num2 + Result;
             }
-
-            goto start;
-
+            Console.WriteLine("Collection is :" + Result);
+            Console.ReadKey();
         }
     }
 }

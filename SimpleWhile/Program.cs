2793eaa0733566a57894255c52f4053fa41b2baf
Berat Karabulut <53434685+Inqusiter@users.noreply.github.com>
2021-12-29 20:14:40 +0300

    Add files via upload

---
+++ SimpleWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * İf condition is true.
+             * {} code block to be executed.
+             * 
+             */
+            int i = 1;
+            while (i<10)
+            {
+                Console.WriteLine("Hey!");
+                i = i + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
